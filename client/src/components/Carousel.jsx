0da256ae24d2040d01543d65940690e09cd50cf7
Natnael Del Rosario Daly <96176909+BurquerRayon@users.noreply.github.com>
2025-08-01 02:22:53 -0400

    modified:   client/src/components/Carousel.jsx 	modified:   client/src/styles/Carrusel.css

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Carrusel.css";
 
+// Array of images for the carousel
 const images = [
-  "/assets/img/Instituto/e1.jpeg",
-  "/assets/img/Instituto/e2.jpeg",
-  "/assets/img/Instituto/e4.jpg",
-  "/assets/img/Instituto/e6.jpg",
+  { src: "/assets/img/Instituto/e1.jpeg", label: "Area de regreso del tour" },
+  {
+    src: "/assets/img/Instituto/e2.jpeg",
+    label: "Arboles caoba, camino de inicio del tour",
+  },
+  { src: "/assets/img/Instituto/e4.jpg", label: "Cueva" },
+  { src: "/assets/img/Instituto/e6.jpg", label: "Laberinto" },
 ];
 
 const Carrusel = () => {
@@ -18,13 +22,14 @@ const Carrusel = () => {
     return () => clearInterval(interval);
   }, []);
 
-return (
-  <div
-    className="carousel-container"
-    style={{ backgroundImage: `url(${images[currentIndex]})` }}
-  />
-);
-
+  return (
+    <div
+      className="carousel-container"
+      style={{ backgroundImage: `url(${images[currentIndex].src})` }}
+    >
+      <div className="carousel-label">{images[currentIndex].label}</div>
+    </div>
+  );
 };
 
 export default Carrusel;
